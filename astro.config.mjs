ed8b221d76cce996a2035e17a40289b8171a741d
Anton <anton@primeappliancerepair.com>
2026-04-14 05:31:09 +0000

    Fix site URL: primerepair.net (was primeappliancerepair.com)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://primeappliancerepair.com',
+  site: 'https://primerepair.net',
   compressHTML: true,
   integrations: [sitemap()],
 });
